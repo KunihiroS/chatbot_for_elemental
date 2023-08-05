0affc2b88ba564ca4c0f952d6a90dd4448bebe49
KunihiroS <kunihiros@gmail.com>
2023-08-05 23:56:24 +0900

    Update streamlit_app.py
    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import random
 
 st.title('ふしぎなこの子になんでもきいてみよう！！')
 
-user_input = st.text_input("なんでもいれててね")
+user_input = st.text_input("なんでもきいてね")
 st.write("いれたもじ: ", user_input)
 
 responses = ["That's interesting!", "Tell me more.", "I see.", "Very cool."]

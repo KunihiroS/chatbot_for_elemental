b7a580a4395aa501717eb3e77973d14bd204a5bf
KunihiroS <kunihiros@gmail.com>
2023-08-05 23:55:42 +0900

    Update streamlit_app.py
    random

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
+import random
 
-st.title('Hello, Streamlit!')
+st.title('ふしぎなこの子になんでもきいてみよう！！')
 
-user_input = st.text_input("なんでも入力してね")
-st.write("入力: ", user_input)
+user_input = st.text_input("なんでもいれててね")
+st.write("いれたもじ: ", user_input)
+
+responses = ["That's interesting!", "Tell me more.", "I see.", "Very cool."]
+if user_input:
+    st.write(random.choice(responses))

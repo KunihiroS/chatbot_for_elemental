7909245b2b78d8ffbfa01ef32bee99d565f1fb07
KunihiroS <kunihiros@gmail.com>
2023-08-06 23:32:07 +0900

    Update streamlit_app.py
    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,19 +18,19 @@ with st.form(key='chat_form'):
    
     if send_button and user_input and user_input != st.session_state.last_input:
         # OpenAI APIを使用して応答を取得
-	try:
-    		response = openai.Completion.create(
-      			model="gpt-3.5-turbo",
-      			messages=[
-		        	{"role": "system", "content": "You are a helpful tutor for Japanese elemetary school students."},
-           			{"role": "user", "content": user_input}
-       			]
-    		)
-    	bot_response = response['choices'][0]['message']['content']
-	except openai.error.OpenaiError as e:
-    		bot_response = str(e)
-	
-        st.session_state.chat_log.insert(0, ("Bot", bot_response ))
+        try:
+            response = openai.Completion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a helpful tutor for Japanese elemetary school students."},
+                    {"role": "user", "content": user_input}
+                ]
+            )
+            bot_response = response['choices'][0]['message']['content']
+        except openai.error.OpenaiError as e:
+            bot_response = str(e)
+        
+        st.session_state.chat_log.insert(0, ("Bot", bot_response))
         st.session_state.chat_log.insert(0, ("User", user_input))
         st.session_state.last_input = user_input
         

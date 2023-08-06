13d4235ff57907d612f22dfe459300dd8e189e26
KunihiroS <kunihiros@gmail.com>
2023-08-06 13:46:52 +0900

    Update streamlit_app.py
    session

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,19 @@ import random
 st.title('ふしぎなこの子になんでもきいてみよう！！')
 
 responses = ["That's interesting!", "Tell me more.", "I see.", "Very cool."]
-chat_log = []
+
+if 'chat_log' not in st.session_state:
+    st.session_state.chat_log = []
 
 with st.form(key='chat_form'):
     user_input = st.text_input("なんでもきいてね")
     send_button = st.form_submit_button("送信")
    
 if send_button and user_input:
-    chat_log.append(("User", user_input))
-    chat_log.append(("Bot", random.choice(responses)))
+    st.session_state.chat_log.append(("User", user_input))
+    st.session_state.chat_log.append(("Bot", random.choice(responses)))
 
-for role, message in chat_log:
+for role, message in st.session_state.chat_log:
     if role == "User":
         st.write(f'<div style="text-align: left; padding: 10px; border-radius: 5px; background-color: #191970;">{message}</div>', unsafe_allow_html=True)
     else:

4e4aeab12765b6232a2c9bbbc64f941fc1dff344
KunihiroS <kunihiros@gmail.com>
2023-08-06 13:54:49 +0900

    Update streamlit_app.py
    reverse

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,12 @@ responses = ["That's interesting!", "Tell me more.", "I see.", "Very cool."]
 if 'chat_log' not in st.session_state:
     st.session_state.chat_log = []
 
+for role, message in reversed(st.session_state.chat_log):
+    if role == "User":
+        st.write(f'<div style="text-align: left; padding: 10px; border-radius: 5px; background-color: #191970;">{message}</div>', unsafe_allow_html=True)
+    else:
+        st.write(f'<div style="text-align: right; padding: 10px; border-radius: 5px; background-color: #B1063A;">{message}</div>', unsafe_allow_html=True)
+
 with st.form(key='chat_form'):
     user_input = st.text_input("なんでもきいてね")
     send_button = st.form_submit_button("送信")
@@ -15,9 +21,3 @@ with st.form(key='chat_form'):
 if send_button and user_input:
     st.session_state.chat_log.append(("User", user_input))
     st.session_state.chat_log.append(("Bot", random.choice(responses)))
-
-for role, message in st.session_state.chat_log:
-    if role == "User":
-        st.write(f'<div style="text-align: left; padding: 10px; border-radius: 5px; background-color: #191970;">{message}</div>', unsafe_allow_html=True)
-    else:
-        st.write(f'<div style="text-align: right; padding: 10px; border-radius: 5px; background-color: #B1063A;">{message}</div>', unsafe_allow_html=True)

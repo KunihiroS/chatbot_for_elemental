daf553e38356bc1b95af707509df70dc24d5ecec
KunihiroS <kunihiros@gmail.com>
2023-08-06 14:34:28 +0900

    Update streamlit_app.py
    yew

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,12 +11,6 @@ if 'chat_log' not in st.session_state:
 if 'last_input' not in st.session_state:
     st.session_state.last_input = ""
 
-for role, message in st.session_state.chat_log:
-    if role == "User":
-        st.write(f'<div style="text-align: left; padding: 10px; border-radius: 5px; background-color: #191970;">{message}</div>', unsafe_allow_html=True)
-    else:
-        st.write(f'<div style="text-align: right; padding: 10px; border-radius: 5px; background-color: #B1063A;">{message}</div>', unsafe_allow_html=True)
-
 with st.form(key='chat_form'):
     user_input = st.text_input("なんでもきいてね")
     send_button = st.form_submit_button("送信")
@@ -25,3 +19,9 @@ with st.form(key='chat_form'):
         st.session_state.chat_log.append(("User", user_input))
         st.session_state.chat_log.append(("Bot", random.choice(responses)))
         st.session_state.last_input = user_input
+        
+for role, message in st.session_state.chat_log:
+    if role == "User":
+        st.write(f'<div style="text-align: left; padding: 10px; border-radius: 5px; background-color: #191970;">{message}</div>', unsafe_allow_html=True)
+    else:
+        st.write(f'<div style="text-align: right; padding: 10px; border-radius: 5px; background-color: #B1063A;">{message}</div>', unsafe_allow_html=True)
